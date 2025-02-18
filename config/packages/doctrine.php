cda5090d5eb327c57306eedcc248cbcf10672a97
Jenne van der Meer <jennevdmeer@impulze.net>
2025-02-18 14:55:37 +0100

    Without DoctrineConfig builder

--- config/packages/doctrine.php
+++ config/packages/doctrine.php
@@ -2,33 +2,50 @@
 
 declare(strict_types=1);
 
-use Symfony\Config\DoctrineConfig;
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-return static function (DoctrineConfig $doctrine) {
-    $doctrine->dbal()->connection('lens_api')->url('%env(resolve:LENS_API_DATABASE_URL)%');
+return static function (ContainerConfigurator $container): void {
+    $container->extension('doctrine', [
+        'dbal' => [
+            'connections' => [
+                'lens_api' => [
+                    'url' => env('LENS_API_DATABASE_URL'),
+                ],
+            ],
+        ],
+        'orm' => [
+            'entity_managers' => [
+                'lens_api' => [
+                    'naming_strategy' => \Lens\Bundle\LensApiBundle\Doctrine\NamespacedUnderscoreNamingStrategy::class,
+                    'connection' => 'lens_api',
+                    'mappings' => [
+                        'Lens\Bundle\LensApiBundle' => [
+                            'is_bundle' => false,
+                            'type' => 'attribute',
+                            'dir' => param('kernel.project_dir').'/vendor/lensmedia/api.lensmedia.nl-bundle/src/Entity',
+                            'prefix' => 'Lens\Bundle\LensApiBundle\Entity',
+                        ],
+                    ],
 
-    $em = $doctrine->orm()->entityManager('lens_api');
-
-    $em->namingStrategy(\Lens\Bundle\LensApiBundle\Doctrine\NamespacedUnderscoreNamingStrategy::class)
-        ->connection('lens_api');
-
-    $em->mapping('Lens\Bundle\LensApiBundle')
-        ->isBundle(false)
-        ->type('attribute')
-        ->dir(dirname(__DIR__, 2).'/src/Entity')
-        ->prefix('Lens\Bundle\LensApiBundle\Entity');
-
-    $em->dql()
-        ->stringFunction('field', \DoctrineExtensions\Query\Mysql\Field::class)
-        ->stringFunction('hex', \DoctrineExtensions\Query\Mysql\Hex::class)
-
-        ->numericFunction('acos', \DoctrineExtensions\Query\Mysql\Acos::class)
-        ->numericFunction('cos', \DoctrineExtensions\Query\Mysql\Cos::class)
-        ->numericFunction('radians', \DoctrineExtensions\Query\Mysql\Radians::class)
-        ->numericFunction('sin', \DoctrineExtensions\Query\Mysql\Sin::class)
-        ->numericFunction('point', \Lens\Bundle\LensApiBundle\Doctrine\Functions\Point::class)
-        ->numericFunction('st_distance_sphere', \Lens\Bundle\LensApiBundle\Doctrine\Functions\STDistanceSphere::class)
-
-        ->datetimeFunction('date', \DoctrineExtensions\Query\Mysql\Date::class)
-    ;
+                    'dql' => [
+                        'string_functions' => [
+                            'field' => \DoctrineExtensions\Query\Mysql\Field::class,
+                            'hex' => \DoctrineExtensions\Query\Mysql\Hex::class,
+                        ],
+                        'numeric_functions' => [
+                            'acos' => \DoctrineExtensions\Query\Mysql\Acos::class,
+                            'cos' => \DoctrineExtensions\Query\Mysql\Cos::class,
+                            'radians' => \DoctrineExtensions\Query\Mysql\Radians::class,
+                            'sin' => \DoctrineExtensions\Query\Mysql\Sin::class,
+                            'point' => \Lens\Bundle\LensApiBundle\Doctrine\Functions\Point::class,
+                            'st_distance_sphere' => \Lens\Bundle\LensApiBundle\Doctrine\Functions\STDistanceSphere::class,
+                        ],
+                        'datetime_functions' => [
+                            'date' => \DoctrineExtensions\Query\Mysql\Date::class,
+                        ],
+                    ],
+                ],
+            ],
+        ]
+    ]);
 };
